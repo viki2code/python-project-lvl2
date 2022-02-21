d3bc429f3873a262918d260111e2654f901e43c5
Viktory <vberkle@gmail.com>
2022-02-21 22:03:26 +0700

    add gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+import argparse
+from gendiff import generate_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', dest='FORMAT',
+                        help='set format of output')
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
+
+if __name__ == '__main__':
+    main()
