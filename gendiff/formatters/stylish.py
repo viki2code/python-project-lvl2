84cd3cff40d7b7729562074bd92c8bac1e3ad26f
Viktory <vberkle@gmail.com>
2022-03-10 00:56:02 +0700

    update tests

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -23,22 +23,22 @@ def stylish(data, replacer=' ', spaces_count=1):
         deep_indent = replacer * deep_indent_size
         current_replacer = replacer * (deep_indent_size - spaces_count)
         lines = []
-        for key, value_dict in current_value.items():
+        for key, value_ in current_value.items():
             # if only copy dictionary
-            if (isinstance(value_dict, dict)
-                and value_dict.get('action') is None) \
-                    or not isinstance(value_dict, dict):
+            is_value_dict = isinstance(value_, dict)
+            if (is_value_dict and value_.get('action') is None) \
+                    or not is_value_dict:
                 sign = ACTION_SIGN['unchanged']
-                value = value_dict
+                value = value_
             else:
-                sign = ACTION_SIGN[value_dict.get('action')]
+                sign = ACTION_SIGN[value_.get('action')]
                 if isinstance(sign, list):
                     old_sign = sign[0]
-                    old_value = value_dict['old_value']
+                    old_value = value_['old_value']
                     lines.append(f'{deep_indent}{old_sign}{replacer}{key}: '
                                  f'{walk(old_value, depth + 2)}')
                     sign = sign[1]
-                value = value_dict['value']
+                value = value_['value']
             lines.append(f'{deep_indent}{sign}{replacer}{key}: '
                          f'{walk(value, depth + 2)}')
         result = itertools.chain("{", lines, [current_replacer + "}"])
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,45 +1,36 @@
 import os
+import pytest
 from gendiff.generate_diff import generate_diff
-from gendiff.formatters.stylish import stylish
-
+from gendiff.formatters.format import STYlISH_FORMAT, PLAIN_FORMAT
 
 FIXTURES_FOLDER = 'fixtures'
 
+FILE_FLAT_FIRST_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_first.json')
+FILE_FLAT_SECOND_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_second.json')
+FILE_FLAT_FIRST_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_first.yml')
+FILE_FLAT_SECOND_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_second.yml')
+FILE_NESTED_FIRST_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_first.json')
+FILE_NESTED_SECOND_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_second.json')
+FILE_NESTED_FIRST_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_first.yml')
+FILE_NESTED_SECOND_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_second.yml')
 
-def test_gendiff_json():
-    file1 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'first_file_test1.json')
-    file2 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'second_file_test1.json')
-    file_result = open(os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_test1.json'))
-    str_result = file_result.read()
-    format_name = 'stylish'
-    assert generate_diff(file1, file2, format_name) == str_result
-    file_result.close()
-
-
-'''def test_gendiff_yaml():
-    file1 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'first_file_test1.json')
-    file2 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'second_file_test2.yml')
-    file_result = open(os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_test2.json'))
-    str_result = file_result.read()
-    assert stylish(generate_diff(file1, file2), ' ', 2) == str_result
-    file_result.close()
-'''
-
-def test_nesteed_gendiff_yaml():
-    file1 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nesteed1.json')
-    file2 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nesteed2.json')
-    format_name = 'stylish'
-    file_result = open(os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nesteed.json'))
-    str_result = file_result.read()
-    assert generate_diff(file1, file2, format_name) == str_result
-    file_result.close()
+RESULT_FLAT_STYLISH = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_flat_stylish.json')
+RESULT_NESTED_STYLISH = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_stylish.txt')
+RESULT_NESTED_PLAIN = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_plain.txt')
 
 
-def test_nesteed_gendiff_yaml():
-    file1 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nesteed1.json')
-    file2 = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nesteed2.json')
-    format_name = 'plain'
-    file_result = open(os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_plain.txt'))
-    str_result = file_result.read()
-    assert generate_diff(file1, file2, format_name) == str_result
-    file_result.close()
+def get_correct_result(path_file):
+    with open(path_file, 'r') as file:
+        result = file.read()
+    return result
+
+
+@pytest.mark.parametrize("first_file, second_file, result_file,format_name", [
+    (FILE_FLAT_FIRST_JSON, FILE_FLAT_SECOND_JSON, RESULT_FLAT_STYLISH, STYlISH_FORMAT),
+    (FILE_FLAT_FIRST_YML, FILE_FLAT_SECOND_YML, RESULT_FLAT_STYLISH, STYlISH_FORMAT),
+    (FILE_NESTED_FIRST_JSON, FILE_NESTED_SECOND_JSON, RESULT_NESTED_STYLISH, STYlISH_FORMAT),
+    (FILE_NESTED_FIRST_YML, FILE_NESTED_SECOND_YML, RESULT_NESTED_STYLISH, STYlISH_FORMAT),
+    (FILE_NESTED_FIRST_JSON, FILE_NESTED_SECOND_JSON, RESULT_NESTED_PLAIN, PLAIN_FORMAT),
+    (FILE_NESTED_FIRST_YML, FILE_NESTED_SECOND_YML, RESULT_NESTED_PLAIN, PLAIN_FORMAT)])
+def test_gendiff(first_file, second_file, result_file, format_name):
+    assert generate_diff(first_file, second_file, format_name) == get_correct_result(result_file)
