7fb0f02ca24fbe681a0c443f0d5c199a38f9de81
Viktory <vberkle@gmail.com>
2022-03-12 09:54:32 +0700

    add test

--- tests/test_parsing_data.py
+++ tests/test_parsing_data.py
@@ -1,5 +1,5 @@
 import os
-from gendiff.data_parser import parsing
+from gendiff.data_parser import parsing, get_file_type
 
 FIXTURES_FOLDER = 'fixtures'
 FILE_FLAT_FIRST_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_first.json')
@@ -12,3 +12,9 @@ def test_parsing():
     assert parsing(open(FILE_FLAT_FIRST_JSON), 'json') == RESULT_DICT
     assert parsing(open(FILE_FLAT_FIRST_YML), 'yml') == RESULT_DICT
     assert parsing(open(FILE_FLAT_FIRST_YML), 'txt') is None
+
+
+def test_file_type():
+    assert get_file_type(FILE_FLAT_FIRST_JSON) == 'JSON'
+    assert get_file_type(FILE_FLAT_FIRST_YML) == 'YML'
+
