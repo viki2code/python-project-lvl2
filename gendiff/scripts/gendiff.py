afe8fc433b1632327b5386922f5c3d662b32bc2a
Viktory <vberkle@gmail.com>
2022-03-14 16:31:37 +0700

    test

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import argparse
 from gendiff.generate_diff import generate_diff
 
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,24 +4,28 @@ from gendiff.generate_diff import generate_diff
 from gendiff.formatters.format import STYlISH_FORMAT, PLAIN_FORMAT, JSON_FORMAT
 import typing
 
-
 FIXTURES_FOLDER = 'fixtures'
 
-FILE_FLAT_FIRST_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_first.json')
-FILE_FLAT_SECOND_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_second.json')
-FILE_FLAT_FIRST_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_first.yml')
-FILE_FLAT_SECOND_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_flat_second.yml')
-FILE_NESTED_FIRST_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_first.json')
-FILE_NESTED_SECOND_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_second.json')
-FILE_NESTED_FIRST_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_first.yml')
-FILE_NESTED_SECOND_YML = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'file_nested_second.yml')
 
-RESULT_FLAT_STYLISH = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_flat_stylish.txt')
-RESULT_FLAT_PLAIN = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_flat_plain.txt')
-RESULT_FLAT_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_flat_json.txt')
-RESULT_NESTED_STYLISH = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_stylish.txt')
-RESULT_NESTED_PLAIN = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_plain.txt')
-RESULT_NESTED_JSON = os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, 'result_nested_json.json')
+def get_path(file_name):
+    return os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER, file_name)
+
+
+FILE_FLAT_FIRST_JSON = get_path('file_flat_first.json')
+FILE_FLAT_SECOND_JSON = get_path('file_flat_second.json')
+FILE_FLAT_FIRST_YML = get_path('file_flat_first.yml')
+FILE_FLAT_SECOND_YML = get_path('file_flat_second.yml')
+FILE_NESTED_FIRST_JSON = get_path('file_nested_first.json')
+FILE_NESTED_SECOND_JSON = get_path('file_nested_second.json')
+FILE_NESTED_FIRST_YML = get_path('file_nested_first.yml')
+FILE_NESTED_SECOND_YML = get_path('file_nested_second.yml')
+
+RESULT_FLAT_STYLISH = get_path('result_flat_stylish.txt')
+RESULT_FLAT_PLAIN = get_path('result_flat_plain.txt')
+RESULT_FLAT_JSON = get_path('result_flat_json.txt')
+RESULT_NESTED_STYLISH = get_path('result_nested_stylish.txt')
+RESULT_NESTED_PLAIN = get_path('result_nested_plain.txt')
+RESULT_NESTED_JSON = get_path('result_nested_json.json')
 
 
 def get_correct_result(path_file):
@@ -44,8 +48,7 @@ def get_correct_result(path_file):
     (FILE_NESTED_FIRST_JSON, FILE_NESTED_SECOND_JSON, RESULT_NESTED_JSON, JSON_FORMAT),
     (FILE_NESTED_FIRST_YML, FILE_NESTED_SECOND_YML, RESULT_NESTED_JSON, JSON_FORMAT)])
 def test_gendiff(first_file, second_file, result_file, format_name):
+    assert isinstance(generate_diff, typing.Callable)
     assert generate_diff(first_file, second_file, format_name) == get_correct_result(result_file)
 
 
-def test_gendiff_type():
-    assert isinstance(generate_diff, typing.Callable)
